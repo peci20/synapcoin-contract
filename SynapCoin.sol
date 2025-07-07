356597782cce7353d2c6cd06b62b39f2f1453ff7
Promaxstudio <48492440+peci20@users.noreply.github.com>
2025-07-08 00:46:22 +0200

    Update SynapCoin.sol

--- SynapCoin.sol
+++ SynapCoin.sol
@@ -5,8 +5,9 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SynapCoin is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("SynapCoin", "SYN") {
+contract SynapCoin is ERC20("SynapCoin", "SYN"), ERC20Burnable, Ownable {
+    constructor() public Ownable(msg.sender) {
+        // Mint 20,000,000,000 SYN tokens to the contract deployer's address
         _mint(msg.sender, 20_000_000_000 * 10 ** decimals());
     }
 }
